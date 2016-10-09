666740ee4b7637a6522664823c82d56cdef3e829
Ahad Bokhari <ahadbokhari@gmail.com>
2016-10-09 12:56:54 -0500

    changed loop struct

---
+++ app.js
@@ -0,0 +1,3 @@
+const objValues = require('./index');
+
+console.log(objValues({a:1, b:2, c:3, d: {a:'nested'} }));
--- index.js
+++ index.js
@@ -14,22 +14,27 @@ module.exports = objValues;
 
 function objValues(obj, options) {
   options = getinit(options);
-  const keys = Object.keys(obj);
   const val = [];
+  let key;
 
   if (Array.isArray(obj)) {
     throw new Error('Expects object as paramater');
   }
 
   if (options.reverse === true) {
-    for (var i = 0; i < keys.length; i++) {
-      val.push(obj[keys[i]]);
+    for (key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        val.push(obj[key]);
+      }
     }
     return val.reverse();
   } else {
-    for (i = 0; i < keys.length; i++) {
-      val.push(obj[keys[i]]);
+    for (key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        val.push(obj[key]);
+      }
     }
     return val;
   }
 }
+
